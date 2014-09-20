47f1aa0e4b8bc1563fe9d1fa00d4bee27229c32b
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-09-20 16:12:23 -0400

    added some time additions

--- algorithm.js
+++ algorithm.js
@@ -2,7 +2,16 @@
 var now = new Date();
 console.log(now.getHours()+' '+now.getUTCMinutes()+' '+now.getUTCSeconds());
 
-console.log((new Date()).getUTCDate());
+var output = [];
 
+//var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+
+for(var i = 4; i<8; i++){
+	if(i>4){
+		output[i-5] = (new Date(now.getYear(), now.getMonth(), now.getDay(), now.getHours()+i, now.getMinutes()+i, now.getSeconds()));
+	}
+}
+
+console.log(output);
 
 })();
