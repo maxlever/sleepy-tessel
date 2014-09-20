0b90ebfbad3d42b54e3865c6b6aafc920c191f13
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-09-20 16:57:05 -0400

    calculation updates
    encapsulation and exporting of module, wake up time calculations should work now

--- algorithm.js
+++ algorithm.js
@@ -1,17 +1,39 @@
-;(function(){
-var now = new Date();
-console.log(now.getHours()+' '+now.getUTCMinutes()+' '+now.getUTCSeconds());
+//this function calculates the 90 minute cycle from input time and returns a new date object
+var add90 = function(inputDate){
+	var mins = inputDate.getMinutes(),
+			hours = inputDate.getHours();
 
-var output = [];
+	mins = mins + 90;
+
+	if (mins > 59) {
+		mins = mins-59;
+		hours++;
+	}
+
+	var outputDate = new Date(inputDate.toDateString());
+	outputDate.setHours(hours);
+	outputDate.setMinutes(mins);
+	return outputDate;
+}
+
+exports.getWakeUpTimes = function(){
+
+var now = new Date(),
+		last;
 
 //var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+var output = [];
 
-for(var i = 4; i<8; i++){
-	if(i>4){
-		output[i-5] = (new Date(now.getYear(), now.getMonth(), now.getDay(), now.getHours()+i, now.getMinutes()+i, now.getSeconds()));
+//this loops through the number of cycles.
+for(var i = 0; i<5; i++){
+	var shiftedDate = add90(last || now);
+	delete last;
+	last = shiftedDate;
+	if(i>1){
+		output[i-2] = shiftedDate;
 	}
 }
 
-console.log(output);
+	return output;
 
-})();
+}
