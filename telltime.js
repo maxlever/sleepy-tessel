ae313d2d196b07ae41796748ff9e1742dbbbbe2c
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-09-21 03:23:02 -0400

    alarm now works

--- telltime.js
+++ telltime.js
@@ -3,9 +3,21 @@
 var tessel = require('tessel');
 
 var gpio = tessel.port['GPIO']; // select the GPIO port
-var myPin = gpio.pin['G6'];
+var led = gpio.pin['G5'];
 
 
+// var f = 2000;
+// while(f>1000){
+// 	gpio.pwmFrequency(f);
+// 	gpio.pin['G5'].pwmDutyCycle(0.5);
+// 	gpio.pin['G5'].output(1);
+// 	f--;
+// }
+var buzzer = gpio.pin['G5'];
+
+// setInterval(function(){
+// 	console.log(buzzer.read());
+// }, 3000)
 
 //uncomment this if you want to test locally.
 // myPin = {
@@ -14,20 +26,73 @@ var myPin = gpio.pin['G6'];
 // 	}
 // }
 
-myPin.output(1);
-myPin.output(0);
+buzzer.output(1);
+led.output(1);
+led.output(0);
+gpio.digital[1].write(1);
+gpio.digital[1].write(0);
+
 console.log('Fired one output');
 
-setTimeout(function(){
-	console.log('alarm rang')
-	setInterval(function(){
-				gpio.pin['G5'].output(1);
 
-			myPin.output(1);
-			// myPin.output(0);
-	}, 1000);
+var http = require('http');
 
-}, 4000);
+var statusCode = 200;
+var alarm = false;
+var json;
+buzzer.output(0);
+
+setInterval(function start () {
+	console.log('immediate started');
+  http.get('http://10.20.85.89:9929/api/wake', function (res) {
+    console.log('http get initiated');
+    var bufs = [];
+    res.on('data', function (data) {
+      bufs.push(new Buffer(data));
+      json = JSON.parse((new Buffer(data)).toString());
+      console.log(json);
+    });
+
+    res.on('end', function () {
+    	console.log('http get resolved')
+    	if(json.ringing){
+    		alarm = true;
+    	}else{
+    		buzzer.output(0);
+				led.output(0);
+				gpio.digital[1].write(0);
+    		alarm = false;
+    	}
+    	console.log('alarm:'+alarm);
+    	 	// setImmediate(start);
+    });
+
+  }).on('error', function (e) {
+    // setImmediate(start);
+    console.log(e);
+  });
+}, 5000);
+
+var rmint;
+setInterval(function(){
+	if(alarm){
+		console.log('alarm rang');
+		
+		rmint = setInterval(function(){
+			gpio.pwmFrequency(2000);
+			buzzer.pwmDutyCycle(0.5);
+		}, 2000);
+
+				led.output(1);
+				gpio.digital[1].write(1);
+	}else{
+		if(rmint){clearInterval(rmint)};
+		console.log('alarm did not ring');
+				buzzer.output(0);
+				led.output(0);
+				gpio.digital[1].write(0);
+	}
+}, 6000);
 
 
 //crappy code
